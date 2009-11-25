15272844de1fd2f6054ae66ec46c1ce95453d463
matbury <matbury@a09b2584-ff9f-11dd-802b-177316e4ac06>
2009-11-25 01:51:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/flv/db/upgrade.php

--- flv/db/upgrade.php
+++ flv/db/upgrade.php
@@ -2,14 +2,6 @@
 
 // This file keeps track of upgrades to the flv module
 //
-// Sometimes, changes between versions involve alterations to database structures and other
-// major things that may break installations.
-//
-// The upgrade function in this file will attempt to perform all the necessary actions to upgrade
-// your older installation to the current version.
-//
-// If there's something it cannot do itself, it will tell you what you need to do.
-//
 // The commands in here will all be database-neutral, using the functions defined in lib/ddllib.php
 
 function xmldb_flv_upgrade($oldversion=0) {
@@ -17,22 +9,12 @@ function xmldb_flv_upgrade($oldversion=0) {
     global $CFG, $THEME, $db;
 
     $result = true;
-/*
-/// And upgrade begins here. For each one, you'll need one block of code similar to the next one. 
-/// Please, delete these comment lines once this file start handling proper upgrade code.
 
-/// if ($result && $oldversion < YYYYMMDD00) { //New version in version.php
-///     $result = result of "/lib/ddllib.php" function calls
-/// }
-
-/// Lines below (this included)  MUST BE DELETED once you get the first version
-/// of your module ready to be installed. They are here only
-/// for demonstrative purposes and to show how the flv
-/// iself has been upgraded.
+	//if ($result && $oldversion < 2009112500) { //New version in version.php
+	//     $result = result of "/lib/ddllib.php" function calls
+	//}
 
 /// For each upgrade block, the file flv/version.php
-/// needs to be updated . Such change allows Moodle to know
-/// that this file has to be processed.
 
 /// To know more about how to write correct DB upgrade scripts it's
 /// highly recommended to read information available at:
@@ -41,7 +23,7 @@ function xmldb_flv_upgrade($oldversion=0) {
 /// PHP generation posibilities.
 
 /// First example, some fields were added to the module on 20090900
-    if ($result && $oldversion < 2009012800) {
+    /*if ($result && $oldversion < 2009012800) {
 
     /// Define field course to be added to flv
         $table = new XMLDBTable('flv');
@@ -63,12 +45,12 @@ function xmldb_flv_upgrade($oldversion=0) {
         $field->setAttributes(XMLDB_TYPE_INTEGER, '4', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'intro');
     /// Launch add field introformat
         $result = $result && add_field($table, $field);
-    }
+    }*/
 
 /// Second example, some hours later, the same day 20090901
 /// two more fields and one index were added (note the increment
 /// "01" in the last two digits of the version
-    if ($result && $oldversion < 2009012801) {
+    /*if ($result && $oldversion < 2009012801) {
 
     /// Define field timecreated to be added to flv
         $table = new XMLDBTable('flv');
@@ -108,21 +90,8 @@ function xmldb_flv_upgrade($oldversion=0) {
     /// Now the view action
         $rec->action = 'view';
         $result = insert_record('log_display', $rec);
-    }
-
-/// And that's all. Please, examine and understand the 3 example blocks above. Also
-/// it's interesting to look how other modules are using this script. Remember that
-/// the basic idea is to have "blocks" of code (each one being executed only once,
-/// when the module version (version.php) is updated.
-
-/// Lines above (this included) MUST BE DELETED once you get the first version of
-/// yout module working. Each time you need to modify something in the module (DB
-/// related, you'll raise the version and add one upgrade block here.
-
-/// Final return of upgrade result (true/false) to Moodle. Must be
-/// always the last line in the script
-    return $result;
-	*/
+    }*/
+	
+	return $result;
 }
-
 ?>
